258223a1998a1dc0d5033069e317c722aac6ea55
BenSeventy9 <52075695+BenSeventy9@users.noreply.github.com>
2022-09-08 19:14:00 +0200

    Create install_box64.sh

---
+++ install_box64.sh
@@ -0,0 +1,8 @@
+#!/bin/sh
+
+git clone https://github.com/ptitSeb/box64
+cd box64
+mkdir build; cd build; cmake .. -DRK3399=1 -DCMAKE_BUILD_TYPE=RelWithDebInfo
+make -j4
+sudo make install
+sudo systemctl restart systemd-binfmt
