262c0ed29f64f193f88837722a2fe89adf4502e5
BenSeventy9 <52075695+BenSeventy9@users.noreply.github.com>
2022-09-09 13:02:07 +0200

    Update install_box64.sh
    Add: sudo apt install git build-essential cmake

--- install_box64.sh
+++ install_box64.sh
@@ -1,5 +1,6 @@
 #!/bin/sh
 
+sudo apt install git build-essential cmake
 git clone https://github.com/ptitSeb/box64
 cd box64
 mkdir build; cd build; cmake .. -DRK3399=1 -DCMAKE_BUILD_TYPE=RelWithDebInfo
